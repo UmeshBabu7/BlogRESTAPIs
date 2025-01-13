acc3917ed9458948309eacd668388808a233dc70
Umesh <umeshtamang129@gmail.com>
2025-01-13 08:10:17 +0545

    create a first views and urls

--- blogproject/main/urls.py
+++ blogproject/main/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from .import views
 
 urlpatterns = [
+    path('',views.blog_home,name="home"),
+    path('blog_detail/',views.blog_detail,name='blog_detail'),
+    path('user_profile/',views.profile,name='user_profilr')
     
 ]
