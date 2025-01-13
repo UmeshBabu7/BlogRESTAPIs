a79c24aae8ab531681f38d976b30d741d985b730
Umesh <umeshtamang129@gmail.com>
2025-01-13 10:36:20 +0545

    create a model and admin

--- blogproject/main/models.py
+++ blogproject/main/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
+from datetime import date
 
 # Create your models here.
+
+class Blog(models.Model):
+    name = models.CharField(max_length=100)
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    description = models.TextField(help_text="Write your blog")
+    post_date = models.DateField(default=date.today)
+
+    def __str__(self):
+        return self.name + " ==> " + str(self.author)
+
+
+class BlogComment(models.Model):
+    description = models.TextField(help_text="Write your comment")
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    comment_date = models.DateTimeField(auto_now_add=True)
+    blog = models.ForeignKey(Blog, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return str(self.blog)
