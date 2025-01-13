9ce9dae0bf0899523d00ecac26979b2ef29ef51a
Umesh <umeshtamang129@gmail.com>
2025-01-13 14:31:59 +0545

    Created CustiomUserRegistrationForm

--- blogproject/authors/views.py
+++ blogproject/authors/views.py
@@ -1,20 +1,21 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
+from .forms import SignupForm
 
 # Create your views here.
 
 def signUp(request):
     if request.method == "POST":
-        form=UserCreationForm(request.POST)
+        form=SignupForm(request.POST)
         if form.is_valid():
             form.save()
         messages.success(request,"Your account is created successfully")
         return redirect('home')
     else:
         messages.error(request, "Error")
-        
-        form=UserCreationForm()
+
+        form=SignupForm()
     return render(request, "register.html",{'form':form})
 
 
