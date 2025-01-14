4f42e5bb93a31f926aedd7fe88bdfcb81578a7dc
Umesh <umeshtamang129@gmail.com>
2025-01-14 15:07:51 +0545

    Created user login form

--- blogproject/authors/urls.py
+++ blogproject/authors/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from .import views
+from unicodedata import name
 
 urlpatterns = [
     path('create-new-account/', views.signUp, name="register"),
     path('login/', views.logIn, name="login"),
+    path('logout/', views.logOut, name="logout")
 ]
 
--- blogproject/authors/views.py
+++ blogproject/authors/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render,redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm,AuthenticationForm
 from django.contrib import messages
 from .forms import SignupForm
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login,logout
 
 # Create your views here.
 
@@ -27,4 +27,27 @@ def signUp(request):
 
 
 def logIn(request):
-    return render(request, "login.html")
+    if request.method == "POST":
+        form = AuthenticationForm(request, data = request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            
+            user = authenticate(username = username, password=password)
+            
+            if user is not None:
+                login(request, user)
+                messages.success(request, f"You are logged in as {username}")
+                return redirect('home')
+            else:
+                messages.error(request, "Error")
+        else:
+            messages.error(request, "Username or password incorrect")
+    form = AuthenticationForm()
+    return render(request, "login.html", {"login_form": form})
+
+
+def logOut(request):
+    logout(request)
+    messages.success(request, "You have successfully logged out.")
+    return redirect('home')
