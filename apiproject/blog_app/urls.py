390dc68b1c9ec4e37f3ee1f0dea5f23696660a81
Umesh <umeshtamang129@gmail.com>
2025-01-17 17:15:50 +0545

    apiproject/first drf program

---
+++ apiproject/blog_app/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+
+from .import views
+
+urlpatterns = [
+    path('blog_list/',views.blog_list,name='blog_list')
+    
+]
--- apiproject/blog_app/views.py
+++ apiproject/blog_app/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 
 # Create your views here.
+
+def blog_list(request):
+    data={
+        "messages":"Hello first drf program"
+    }
+    return JsonResponse(data)
