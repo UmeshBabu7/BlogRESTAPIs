c58dba3d7e0c7317ffb1822c1a4a94160229a8e7
Umesh <umeshtamang129@gmail.com>
2025-01-19 18:43:25 +0545

    json response

--- apiproject/blog_app/urls.py
+++ apiproject/blog_app/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from .import views
 
 urlpatterns = [
-    path('blog_list/',views.blog_list,name='blog_list')
+    path('blog_list/',views.blog_list,name='blog_list'),
+    path('blog_detail/<int:pk>/',views.blog_detail,name='blog_detail')
     
 ]
--- apiproject/blog_app/views.py
+++ apiproject/blog_app/views.py
@@ -1,10 +1,27 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from .models import Blog
 
 # Create your views here.
 
 def blog_list(request):
-    data={
-        "messages":"Hello first drf program"
+    blogs=Blog.objects.all()
+
+    context={
+        "Blogs":list(blogs.values())
     }
-    return JsonResponse(data)
+    
+    return JsonResponse(context)
+
+
+def blog_detail(request,pk):
+    blogs=Blog.objects.get(pk=pk)
+
+    context={
+        "name":blogs.name,
+        "description":blogs.description,
+        "slug":blogs.slug
+    }
+
+    return JsonResponse(context)
+
