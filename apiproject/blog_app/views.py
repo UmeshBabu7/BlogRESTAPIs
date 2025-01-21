f4ba4e3e7c89ebd9d231c60396d09e3f8f381461
Umesh <umeshtamang129@gmail.com>
2025-01-21 10:22:53 +0545

    status code

--- apiproject/blog_app/views.py
+++ apiproject/blog_app/views.py
@@ -4,6 +4,7 @@ from .models import Blog
 from .serializers import BlogSerializer
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from rest_framework import status
 
 # Create your views here.
 
@@ -13,16 +14,16 @@ def blog_list(request):
     if request.method == "GET":
         all_blogs=Blog.objects.filter(is_public=True)
         serializer=BlogSerializer(all_blogs,many=True)
-        return Response(serializer.data)
+        return Response(serializer.data,status=status.HTTP_200_OK)
     
 
     if request.method =="POST":
         serializer=BlogSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data)
+            return Response(serializer.data,status=status.HTTP_201_CREATED)
         else:
-            return Response(serializer.errors)
+            return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
 
     
 
@@ -32,21 +33,21 @@ def blog_detail(request,id):
     if request.method =='GET':
         blog=Blog.objects.get(id=id)
         serializer=BlogSerializer(blog)
-        return Response(serializer.data)
+        return Response(serializer.data,status=status.HTTP_200_OK)
     
     if request.method =='PUT':
         blog=Blog.objects.get(id=id)
         serializer=BlogSerializer(blog,data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data)
+            return Response(serializer.data,status=status.HTTP_200_OK)
         else:
-            return Response(serializer.errors)
+            return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
         
     if request.method =='DELETE':
         blog=Blog.objects.get(id=id)
         blog.delete()
-        return Response()
+        return Response(status=status.HTTP_204_NO_CONTENT)
         
     
     
