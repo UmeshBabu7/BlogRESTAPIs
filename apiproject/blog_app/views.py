9fe268a02588ff5b42bec5cb67304d0c3f198494
Umesh <umeshtamang129@gmail.com>
2025-01-22 15:05:15 +0545

    using lookup_field

--- apiproject/blog_app/views.py
+++ apiproject/blog_app/views.py
@@ -57,6 +57,7 @@ class BlogListGenericView(mixins.ListModelMixin, mixins.CreateModelMixin, generi
 class BlogDetailGenericView(mixins.RetrieveModelMixin,mixins.UpdateModelMixin,mixins.DestroyModelMixin, generics.GenericAPIView):
     queryset = Blog.objects.all()
     serializer_class = BlogSerializer
+    lookup_field = 'slug'
 
     def get(self, request, *args, **kwargs):
         return self.retrieve(request, *args, **kwargs)
