033d8d429906182ce1611e2b1050793eebe89d39
Umesh <umeshtamang129@gmail.com>
2025-01-23 11:30:30 +0545

    Basic Authentication

--- apiproject/apiproject/urls.py
+++ apiproject/apiproject/urls.py
@@ -4,5 +4,5 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/blog/',include('blog_app.urls')),
-    path('api-auth/', include('rest_framework.urls'))
+   
 ]
